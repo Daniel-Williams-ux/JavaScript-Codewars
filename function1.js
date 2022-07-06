710347475e7fd192e93f452bde3028c0340fafa9
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-06 06:15:49 +0100

    Create function1.js

---
+++ function1.js
@@ -0,0 +1,16 @@
+//Implement a function which multiplies two numbers.
+function multiply (num1, num2) {
+//   const num1 = 2;
+//   const num2 = 3;
+  return num1 * num2;
+}
+
+//Sample Tests
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(multiply(2, 3), 6);
+  });
+});
