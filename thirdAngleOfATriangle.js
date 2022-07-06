c17a5bccc88d70790f9bdf999204b9d135337000
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-06 07:19:15 +0100

    Create thirdAngleOfATriangle.js

---
+++ thirdAngleOfATriangle.js
@@ -0,0 +1,20 @@
+/*
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+function otherAngle(a, b) {
+   return 180 - (a+b);
+}
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(otherAngle(30, 60), 90);
+    assert.strictEqual(otherAngle(60, 60), 60);
+    assert.strictEqual(otherAngle(43, 78), 59);
+    assert.strictEqual(otherAngle(10, 20), 150);
+  });
+});
