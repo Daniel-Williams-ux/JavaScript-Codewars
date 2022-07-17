afb356a00c717075bc64450b0343c2c7ad3c843b
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-17 14:05:59 +0100

    Update smallest_int.js

--- smallest_int.js
+++ smallest_int.js
@@ -3,3 +3,17 @@ function smallestIntegers(args) {
 }
 console.log(smallestIntegers([34, 15, 88, 2]))//2
 console.log(smallestIntegers([34, -345, -1, 100]))//-345
+
+
+function maxNumber(num1, num2) {
+  let max;
+  
+  if (num1 > num2) {
+    max = num1;
+  } else {
+    max = num2
+  }
+  return(max);
+}
+
+console.log(maxNumber(400, 200)) //-> 400
