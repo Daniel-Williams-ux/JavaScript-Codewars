9894ca860ba01f232b64aec6e7ebb52fdc218952
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-18 14:11:01 +0100

    Update doubleInteger.js

--- doubleInteger.js
+++ doubleInteger.js
@@ -1,7 +1,7 @@
 //Code as fast as you can! You need to double the integer and return it.
 function doubleInteger(i) {
   // i will be an integer. Double it and return it.
-  return i * i;
+  return i * 2;
 }
 
 describe("Tests", () => {
