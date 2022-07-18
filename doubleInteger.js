73dd0cbaf498f26d766f0b4fa57770fd8d1b9be2
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-18 14:09:23 +0100

    Create doubleInteger.js

---
+++ doubleInteger.js
@@ -0,0 +1,11 @@
+//Code as fast as you can! You need to double the integer and return it.
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * i;
+}
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(doubleInteger(2), 4);
+  });
+});
